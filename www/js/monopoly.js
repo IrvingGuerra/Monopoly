5b45ca129851539d012d85e2b861396ecc74709c
Irving <guerravargasirving@gmail.com>
2019-11-10 23:46:25 -0600

    Se agregaron alertas

--- www/js/monopoly.js
+++ www/js/monopoly.js
@@ -23,12 +23,29 @@ function inizializarTablero(){
 	$(".cartasAzules").width(casillaAncho*2);
 }
 
+function getTarjeta(e,color) {
+	e = e || window.event;
+    var target = e.target || e.srcElement;
+    if ($(target).hasClass('disabled')) {
+    	showAlert("rojo","<strong>Espera!</strong> No puedes escoger una tarjeta todavia.");
+    }else{
+    	
+    }
+}
+
+
 var loopVar = 1;  
 var dado = 1;
-function randomDado() {
-	loopVar = 1;
-	//Mostraremos una animacion para que se vea cool
-	myLoop(); 
+function randomDado(e) {
+	e = e || window.event;
+    var target = e.target || e.srcElement;
+    if ($(target.parentElement).hasClass('disabled')) {
+    	showAlert("rojo","<strong>Espera!</strong> Aun no es tu turno de tirar.");
+    }else{
+    	loopVar = 1;
+		//Mostraremos una animacion para que se vea cool
+		myLoop(); 
+    }
 }
 
 function myLoop() {
@@ -38,20 +55,27 @@ function myLoop() {
 		if (loopVar < 15) {
 		 myLoop();
 		}
-	}, 200)
+	}, 200);
 }
 
 function setImageDado() {
-	console.log("entra");
 	dado = getRandomNum(1, 6);
+	showAlert("verde","<strong>¡Calculando!</strong> Te toco el numero: "+dado);
 	document.getElementById('dado').src = 'img/dado/'+dado+'.png';
 }
-
-
 
 
 function getRandomNum(min, max) {
 	min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function showAlert(color,text) {
+	$('.alert').removeClass('verde');
+	$('.alert').removeClass('rojo');
+	$('.alert').addClass(color);
+	$('.alert > label').html(text);
+	$('.alert').fadeIn();
+	setTimeout(function () {$('.alert').fadeOut();}, 2000)
 }

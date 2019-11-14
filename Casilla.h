e5a675fcf174c4d39d78166a5a16db613f9b9458
Irving Guerra <irvingguerra@Mac-Pro-de-Irving.local>
2019-11-14 17:15:13 -0600

    casillas

--- Casilla.h
+++ Casilla.h
@@ -6,6 +6,11 @@ class Casilla{
 private:
     int numero;
  	int type;	//1 = Casilla Vacia, 2 = Propiedad, 3 = Carta roja, 4 = Carta Azul, 5 = Carcel, 6 = Salida
+
+
+ 	//Valores necesarios para tipo propiedad
+ 	
+ 	bool status; //false = Si es propiedad nadie la compro, true = ya tiene propietario
  	bool inout; //true = el jugador paga, false = el jugador recibe
  	int cantidad; //Se complementa con inout, es la cantidad que dara o recibira
  	int propietario; //Guarda el id del jugador

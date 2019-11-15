fba309152203bc30a46a4b15a58c7e55e0eaede2
Irving Guerra <irvingguerra@Mac-Pro-de-Irving.local>
2019-11-14 18:19:59 -0600

    tablero h

---
+++ Tablero.h
@@ -0,0 +1,14 @@
+#ifndef TABLERO_H
+#define TABLERO_H
+
+#include "Casilla.h"
+
+class Tablero{
+private:
+    Casilla[26] casillas; //Cantidad de casillas que existen
+public:
+    Tablero();
+    ~Tablero();
+};
+
+#endif
